b01a236fd8b3561e0befc2466597335cbc4ac92b
demonlol <48394561+demonlol@users.noreply.github.com>
2019-10-29 08:55:16 -0500

    Fixed petty code formatting

--- entry.js
+++ entry.js
@@ -13,30 +13,27 @@ if(!utilities.TextIsValid(__TOKEN__)) throw(new Error("A token was not supplied!
 require('./commands/ping.js');
 require('./commands/help.js');
 
-client.on('ready', ()=>{
+client.on('ready', () => {
     console.log('The bot is online!');
 });
 
-client.on('reconnecting', ()=>{
+client.on('reconnecting', () => {
     console.log('Disconnected! Attempting to reconnect!');
 });
 
-client.on('disconnect', ()=>{
+client.on('disconnect', () => {
     throw(new Error('Failed to connect to Discord servers...'));
 });
 
-client.on('message', (message)=>{
+client.on('message', (message) => {
     // Command handling
-    if(message.cleanContent.startsWith(__PREFIX__))
-    {
+    if(message.cleanContent.startsWith(__PREFIX__)) {
         const commandName = message.cleanContent.split(' ')[0].substring(__PREFIX__.length);
         const selectedCommand = Command.getCommandByName(commandName);
-        if (selectedCommand)
-        {
+        if (selectedCommand) {
             const args = message.cleanContent.split(' ').shift();
             selectedCommand.func(client, message, args);
         }
-
     }
 });
 
