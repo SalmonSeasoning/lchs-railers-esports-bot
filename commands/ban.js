841491e6dc44b2100938b048b68103d6c664aeea
demonlol <48394561+demonlol@users.noreply.github.com>
2019-10-29 09:39:04 -0500

    made bad ban command. and no you cant edit it sean

--- commands/ban.js
+++ commands/ban.js
@@ -1,1 +1,24 @@
+const Command = require('../command.js'),
+    new_command = new Command("ban", (client, message, args) => {
+        var mentionedPeople = message.mentions;
+        var bannedPeople = 0;
+        var bannedPeopleString = "";
+        for(let i = 0; i < mentionedPeople.length; i++) {
+            let member = mentionedPeople.members.array()[i];
+            if(member.bannable) {
+                bannedPeople++;
+                member.ban();  
+                bannedPeopleString += `<@${member.id}>` + (i == mentionedPeople.length ? '' : '\n');
+            }
+        }
+        const embed = new Discord.RichEmbed()
+            .setTitle("Railer Punishment System - RPS")
+            .setColor(0xf0380e)
+            .setDescription(`**${bannedPeople}** were successfully banned.`)
+            .setFooter("LCHS eSports Bot", "../resources/icon.png")
+            .setThumbnail("../resources/icon.png")
+            .setTimestamp()
+            .addField("Banned Users", bannedPeopleString, false);
+    });
 
+module.exports = new_command;
