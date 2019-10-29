5b01ce6b71b92333131ceda5826187c48014660c
demonlol <48394561+demonlol@users.noreply.github.com>
2019-10-29 08:54:21 -0500

    Fixed petty code formatting

--- command.js
+++ command.js
@@ -1,7 +1,5 @@
-class Command
-{
-    constructor(name, func)
-    {
+class Command {
+    constructor(name, func) {
         if(Command.getCommandByName(name))
             throw(new Error(`A command named ${name} already exists!`));
         this.name = name;
@@ -9,20 +7,17 @@ class Command
         Command.commandList.push(this);
     }
     // Retrieve an array of command names
-    static getCommandNames()
-    {
+    static getCommandNames() {
         let _arr = [];
         Command.commandList.forEach((o)=>_arr.push(o.name));
         return _arr;
     }
     // Retrieve the command with a specific index
-    static getCommandByIndex(index)
-    {
+    static getCommandByIndex(index) {
         return Command.commandList[Math.floor(index)];
     }
     // Retrieve the command index by a name
-    static getCommandIndexByName(name)
-    {
+    static getCommandIndexByName(name) {
         for (let command in Command.commandList) {
             if (Command.commandList[command].name.toLowerCase() == name.toLowerCase()) {
                 return i;
@@ -30,8 +25,7 @@ class Command
         }
     }
     // Retrieve a command (Object) by name
-    static getCommandByName(name)
-    {
+    static getCommandByName(name) {
         for (let command of Command.commandList) {
             if (command.name.toLowerCase() == name.toLowerCase()) {
                 return command;
@@ -39,8 +33,7 @@ class Command
         }
     }
     // Remove a command by index
-    static removeCommandByIndex(index)
-    {
+    static removeCommandByIndex(index) {
         index = Math.floor(index);
         if (!index >= 0 || index >= Command.commandList.length) throw(new Error("Attempting to remove a non-existential command!"));
         for (let i = index; i < Command.commandList.length; i++)
@@ -48,8 +41,7 @@ class Command
         Command.commandList.pop();
     }
     // Remove a command by name
-    static removeCommandByName(name)
-    {
+    static removeCommandByName(name) {
         Command.removeCommandById(Command.getCommandIndexByName(name));
     }
 }
