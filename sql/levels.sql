4c6d89f31f904a849cb5a28dfea4c39fa835968e
iComputer7 <6298231+iComputer7@users.noreply.github.com>
2019-11-04 18:27:35 -0600

    Query that creates the levels table

---
+++ sql/levels.sql
@@ -0,0 +1,8 @@
+USE railer-db;
+CREATE TABLE `levels` (
+    `id` int(6) PRIMARY KEY AUTO_INCREMENT,
+    `user_id` varchar(100) NOT NULL,
+    `xp` int(6) DEFAULT 0 NOT NULL,
+    `last_text_only_sent` varchar(100) NOT NULL,
+    `last_attachment_sent` varchar(100) NOT NULL
+);
