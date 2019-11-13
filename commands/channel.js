123cd1a31eaee7ad6e749e31b9b816e2d14dc799
demonlol <48394561+demonlol@users.noreply.github.com>
2019-11-13 08:30:50 -0600

    Update channel.js

--- commands/channel.js
+++ commands/channel.js
@@ -1,6 +1,52 @@
 const Command = require('../command.js'),
-    new_command = new Command("ping", (client, message, args) => {
-        message.reply(`PONG! ${client.ping}ms!`);
+    new_command = new Command("channel", (client, message, args) => {
+        if(!message.member.hasPermission(0x10))
+            return message.reply('You do not have permission to use this command!');
+        switch(ToLowerCase(args[0]))
+        {
+            case "create":
+                if(ToLowerCase(args[1]) == 'text')
+                {
+                    var _args = args;
+                    _args.shift();
+                    var channelName = args.join('-');
+                    message.guild.createChannel(channelName, {type:'text'}).then(()=>{
+                        message.reply(`**Successfully created #${channelName}**`);
+                    }, ()=>{
+                        message.reply('Failed to create text channel!');
+                    });
+                    
+                }
+                else if(ToLowerCase(args[1]) == 'voice')
+                {
+                    var _args = args;
+                    _args.shift();
+                    var channelName = args.join(' ');
+                    message.guild.createChannel(channelName, {type:'voice'}).then(()=>{
+                        message.reply(`**Successfully created voice channel : ${channelName}**`);
+                    }, ()=>{
+                        message.reply('Failed to create voice channel!');
+                    });
+                }
+                else if(args[1])
+                {
+                    message.reply('Please correctly specify the channel type!');
+                }
+                else
+                {
+                    message.reply('Please specify a channel type!');
+                }
+                break;
+            case "delete":
+                break;
+            default:
+                message.reply('Failed to perform action. Neither "create" nor "delete" specified.');
+        }
     });
+
+function ToLowerCase(x)
+{
+    return x.toLowerCase();
+}
 
 module.exports = new_command;
