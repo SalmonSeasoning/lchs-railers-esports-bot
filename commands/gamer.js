8daddea675259eac834d3da7b242a073ed1d2b53
demonlol <48394561+demonlol@users.noreply.github.com>
2019-11-14 09:22:18 -0600

    Update gamer.js

--- commands/gamer.js
+++ commands/gamer.js
@@ -1,6 +1,6 @@
 const Command = require('../command.js'),
     new_command = new Command("gamer", (client, message, args) => {
-        
+        message.reply(`You are ${Math.floor(Math.random() * 100) + 1}% gamer.`);
     });
 
 module.exports = new_command;
