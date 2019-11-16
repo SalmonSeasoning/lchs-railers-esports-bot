c62964c7e55f9c3de32c88fa9c78f6db15905970
iComputer7 <6298231+iComputer7@users.noreply.github.com>
2019-11-15 20:27:00 -0600

    doesDatabaseExist command

---
+++ commands/dbtestcommands.js
@@ -0,0 +1,8 @@
+const Command = require("../command.js");
+const database = require("../database.js");
+
+module.exports.doesDatabaseExist = new Command("doesdbexist", (client, message, args, db) => {
+    database.doesDatabaseExist(db)
+    .then((res) => {message.channel.send(`Database does ${(res) ? "" : "not"} exist.`)})
+    .catch((e) => {message.channel.send(`Error: ${e}`)});
+}, false, true);
