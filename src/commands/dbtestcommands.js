f95d3dc367ae5d71ef11e78f1b6b30b12ce7cb7c
iComputer7 <6298231+iComputer7@users.noreply.github.com>
2019-11-16 19:13:54 -0600

    DB testing commands can be easily switched to elevated

--- src/commands/dbtestcommands.js
+++ src/commands/dbtestcommands.js
@@ -1,8 +1,13 @@
 const Command = require("../command.js");
 const database = require("../database.js");
 
+//Should these commands require admin?
+//In a testing environment where the database gets wiped frequently, this should be set to false
+//In production, this should be set to true.
+const admin = false;
+
 module.exports.doesDatabaseExist = new Command("doesdbexist", (client, message, args, db) => {
     database.doesDatabaseExist(db)
     .then((res) => {message.channel.send(`Database does ${(res) ? "" : "not "}exist.`)})
     .catch((e) => {message.channel.send(`Error: ${e}`)});
-}, false, true);
+}, admin, true);
