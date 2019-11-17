91f29e84806cb5f935f3f88a6892266222c754b7
iComputer7 <6298231+iComputer7@users.noreply.github.com>
2019-11-16 19:11:21 -0600

    Minor text fixes

--- src/commands/dbtestcommands.js
+++ src/commands/dbtestcommands.js
@@ -3,6 +3,6 @@ const database = require("../database.js");
 
 module.exports.doesDatabaseExist = new Command("doesdbexist", (client, message, args, db) => {
     database.doesDatabaseExist(db)
-    .then((res) => {message.channel.send(`Database does ${(res) ? "" : "not"} exist.`)})
+    .then((res) => {message.channel.send(`Database does ${(res) ? "" : "not "}exist.`)})
     .catch((e) => {message.channel.send(`Error: ${e}`)});
 }, false, true);
