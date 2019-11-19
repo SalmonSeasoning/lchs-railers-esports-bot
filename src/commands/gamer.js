46761b156524d51adf6e5540de9fc35ff64f00d1
demonlol <48394561+demonlol@users.noreply.github.com>
2019-11-19 09:27:37 -0600

    working on gamer command
    ill finish the rest later

--- src/commands/gamer.js
+++ src/commands/gamer.js
@@ -1,6 +1,25 @@
 const Command = require('../command.js'),
-    new_command = new Command("gamer", (client, message, args) => {
-        message.reply(`You are ${Math.floor(Math.random() * 100) + 1}% gamer.`);
+    new_command = new Command("gamer", (client, message, args, db) => {
+        //save metadata for gamer
+        const database = require("../database.js");
+        if(!database.doesCosmeticTableExist(db)) {
+            message.reply('The cosmetic table does not exist. [gamer.js]');    
+        } else {
+            let gamerPercentage = Math.floor(Math.random() * 100) + 1;
+            let userMetadata;
+            database.getUserMetadata(db, user_id).then((res) => {
+                userMetadata = Json.parse(res);
+            }).catch((e) => {
+                userMetadata = undefined;
+            });
+            if(userMetadata == undefined || userMetadata == '') {
+                database.setUserMetadata(db, message.author.id, '{}').then((res) => {
+                    //ill work on this later
+                });
+            }
+            
+            
+        }
     });
 
 module.exports = new_command;
