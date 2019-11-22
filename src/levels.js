fb99634546aab7d91744f4faa4c2014cdb4bca8e
Ty Qualters <12106131+SalmonSeasoning@users.noreply.github.com>
2019-11-21 21:23:58 -0600

    Create levels.js

---
+++ src/levels.js
@@ -0,0 +1,64 @@
+// Usage: Pass the message object, pass the currentLevel, function
+// will return the amount of XP to give the user. Check should be
+// LevelUp(message, 438) > 0
+// before assigning the new level
+function LevelUp(message, currentLevel)
+{
+  let attachment = message.attachments.array()[0];
+  let xp = currentLevel;
+  if(!attachment)
+  {
+  // just a normal message
+    if(message.content.length > 1500)
+    {
+        xp += 2;
+        return xp; // definitely spam
+    }
+    else if (message.content.length > 1000)
+    {
+        xp += 3;
+        return xp;
+    }
+    if(message.content.length > 500)
+    {
+        xp += 5;
+        return xp; // this is ideal since spam and lots of text gets annoying
+    }
+    if(message.content.length > 50)
+    {
+        xp += 3;
+        return xp; // more than 50 characters, probably an actual message lol
+    }
+    if(message.content.length <= 50)
+    {
+      return xp;
+    }
+  }else
+  {
+    xp += 2; // for the attachment
+    if(message.content.length > 1500)
+    {
+        xp += 2;
+        return xp; // definitely spam
+    }
+    else if (message.content.length > 1000)
+    {
+        xp += 3;
+        return xp;
+    }
+    if(message.content.length > 500)
+    {
+        xp += 5;
+        return xp; // this is ideal since spam and lots of text gets annoying
+    }
+    if(message.content.length > 50)
+    {
+        xp += 3;
+        return xp; // more than 50 characters, probably an actual message lol
+    }
+    if(message.content.length <= 50)
+    {
+      return xp;
+    }
+  }
+}
